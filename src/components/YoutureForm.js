191b7e42cae1d3f512c8235039e8850003643f77
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-11 10:55:07 +0530

    managing form state

--- src/components/YoutureForm.js
+++ src/components/YoutureForm.js
@@ -2,16 +2,23 @@ import React from "react";
 import { useFormik } from "formik";
 
 function YoutubeForm() {
-  const formik = useFormik({});
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+  console.log("form values", formik.values);
   return (
     <div>
       <form action="">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} />
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange} />
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" />
+        <input type="text" name="channel" value={formik.values.channel} onChange={formik.handleChange} />
         <button>Submit</button>
       </form>
     </div>
