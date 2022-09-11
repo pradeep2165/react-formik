40c72ec5ba0a914195e9dfec366468ae25970435
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-11 11:06:25 +0530

    handeling submission

--- src/components/YoutureForm.js
+++ src/components/YoutureForm.js
@@ -8,18 +8,21 @@ function YoutubeForm() {
       email: "",
       channel: "",
     },
+    onSubmit: (values) => {
+      console.log("from data", values);
+    },
   });
   console.log("form values", formik.values);
   return (
     <div>
-      <form action="">
+      <form action="" onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name</label>
         <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} />
         <label htmlFor="email">Email</label>
         <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange} />
         <label htmlFor="channel">Channel</label>
         <input type="text" name="channel" value={formik.values.channel} onChange={formik.handleChange} />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
