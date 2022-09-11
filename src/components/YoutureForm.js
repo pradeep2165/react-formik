f676ccb0646f9a0b9589c0764b989ca7dc96ead8
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-11 17:14:45 +0530

    fastfield use to prevent unneccessary render

--- src/components/YoutureForm.js
+++ src/components/YoutureForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage, fieldArray, FieldArray } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray, FastField } from "formik";
 import * as Yup from "yup";
 import TextError from "./TextError";
 
@@ -51,9 +51,9 @@ function YoutubeForm() {
         </div>
         <div className="'from-control">
           <label htmlFor="address">Address</label>
-          <Field name="address">
+          <FastField name="address">
             {(props) => {
-              console.log(props);
+              console.log("Field Render");
               const { field, form, meta } = props;
               return (
                 <div>
@@ -62,7 +62,7 @@ function YoutubeForm() {
                 </div>
               );
             }}
-          </Field>
+          </FastField>
         </div>
         <div className="form-control">
           <label htmlFor="facebook">Facebook profile</label>
@@ -84,7 +84,6 @@ function YoutubeForm() {
           <label>List of Phone Number</label>
           <FieldArray name="phNumbers">
             {(fieldArrayProps) => {
-              console.log("fieldArrayProps", fieldArrayProps);
               const { push, remove, form } = fieldArrayProps;
               const { values } = form;
               const { phNumbers } = values;
